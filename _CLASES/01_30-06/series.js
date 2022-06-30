770032c373eaab1867a1df0c1068f89cf7407a5e
Arbusta <gabriel.munoz@arbusta.net>
2022-06-30 11:54:09 -0300

    Node primer clase

---
+++ _CLASES/01_30-06/series.js
@@ -0,0 +1,7 @@
+const series = [
+    {titulo: 'Mad Men', temporadas: 7},
+    {titulo: 'Breaking Bad', temporadas: 5},
+    {titulo: 'Seinfeld', temporadas: 9},
+    ];
+    
+    module.exports = series;    
