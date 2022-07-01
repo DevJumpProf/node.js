f84c91dbb625a6e8cad5383e44ca86635f11bd4c
VictoriaNadia <victoria.gonzalez@arbusta.net>
2022-07-01 12:17:14 -0300

    Victoria

---
+++ _CLASES/_Equipo/VictoriaGonzalez/Servidor/app.js
@@ -0,0 +1,13 @@
+const http = require("http"); // requiero http
+
+// guardo el servidor en una variable
+const server = http.createServer((req, res) => {
+  console.log("respuesta del servidor funcionando...");
+  res.end("El servidor me devuelve esto. Me llamo Victoria Gonzalez");
+});
+
+const puerto = 3000; // guardo el puerto en una variables
+
+server.listen(puerto, () => {
+  console.log("Servidor Escuchando...");
+});
