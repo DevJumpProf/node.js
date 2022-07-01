fad0100b516c570be579726d3c4189ede325606b
maxiPortelBeraud <maximiliano.portel@arbusta.net>
2022-07-01 15:59:09 -0300

    otras pruebas

--- _CLASES/_Equipo/maxiPortel/Clase2/app.js
+++ _CLASES/_Equipo/maxiPortel/Clase2/app.js
@@ -1,12 +1,28 @@
-const http = require('http'); //modulo nativo
+/* const http = require('http'); //modulo nativo
 
 const server = http.createServer((req,res) => {
-    console.log('Respuesta de servidor completa.');;
+    console.log('Respuesta de servidor completa.');
     res.end('Mi segundo server esta encendido');
 })
 
 const port = 3000;
 
 server.listen(port, () => {
-    console.log('Escuchando pedidos...')
+    console.log('Escuchando pedidos...');
+}); */
+
+const http = require('http');
+
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    console.log('Respondiendo solicitudes');
+    if(req.url == '/contacto'){
+        res.write('Estas en contacto ')
+    }
+    res.end('Hola servidor 2');
+})
+
+server.listen(port, () => {
+    console.log('Servidor on');
 })
