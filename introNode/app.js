9de757ec67f29ba2d497075bf4db512e5fb393f0
Rodríguez Galo <r8driguezgalo@gmail.com>
2022-07-01 11:24:30 -0300

    cambios

---
+++ introNode/app.js
@@ -0,0 +1,7 @@
+const http = require ("http");
+/* http.createServer(function (req, res){
+res.writeHead(200, {"Content-Type": "text/plain"});
+res.end("mi primer server node");
+}).listen(3030, "localHost");
+ */
+
---
+++ introNode/introNode.js
@@ -0,0 +1,5 @@
+const moment = require ("moment") // de terceros
+const fs = require ("fs") // nativo
+const series = require ("./series") // propio
+
+console.log(series)
