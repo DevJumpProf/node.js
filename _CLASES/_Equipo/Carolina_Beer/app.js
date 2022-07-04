1ecf4b6ffed0f76046129b9343c25fb6e36ae338
BeerCarolina <carolina.beer@arbusta.net>
2022-07-04 17:52:32 -0300

    Ejercicio2 Carolina Beer

--- _CLASES/_Equipo/Carolina_Beer/app.js
+++ _CLASES/_Equipo/Carolina_Beer/app.js
@@ -1,12 +1,40 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req,res)=>{
-    console.log('respuesta del servidor funcionando...');
-    res.end('El servidor esta respondiendo');
+// const server = http.createServer((req,res)=>{
+//     console.log('respuesta del servidor funcionando...');
+//     res.end('El servidor esta respondiendo');
+// });
+
+// const puerto = 3030;
+
+// server.listen(puerto, ()=> {
+//     console.log("Servidor Escuchando...");
+// })
+
+const express = require("express"); 
+const app = express();
+const port = 3030; 
+
+
+
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.get("/", (req, res) => {
+    res.render("index", { titulo: "inicio EJS" });
+  });
+
+
+  app.get("/nosotros", (req, res) => {
+    res.render("nosotros", { titulo: "Nosotros EJS" });
+  });
+
+
+  app.use((req, res, next) => {
+    res.status(404).render("404", { h1: "Error 404", h2:"no se encontro la página" });
+  });
+
+  
+app.listen(port, () => {
+  console.log(`Servidor OK http://localhost:${port}`);
 });
-
-const puerto = 3030;
-
-server.listen(puerto, ()=> {
-    console.log("Servidor Escuchando...");
-})
