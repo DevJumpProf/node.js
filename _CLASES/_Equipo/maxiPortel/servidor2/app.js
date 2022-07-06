4fa45fe77de756c2aab5e2373d6f78024ce81384
maxiPortelBeraud <maximiliano.portel@arbusta.net>
2022-07-06 11:06:49 -0300

    router y cambio de nombres a las carpetas main

--- _CLASES/_Equipo/maxiPortel/servidor2/app.js
+++ _CLASES/_Equipo/maxiPortel/servidor2/app.js
@@ -2,21 +2,13 @@ const express = require("express"); // llamamos al modulo express para usarlo
 const app = express(); //llamamos al metodo express en una variable, a partir de ahora vamos a usar la variable
 const port = 3000; // configuramos el puerto, en este caso es estatico
 
-
-app.use(express.static(__dirname + '/public'));
-
 app.set('view engine', 'ejs'); // declaramos que vamos a usar vistas y que son .ejs
 app.set('views', __dirname + '/views'); //decimos que esas vistas estan en una ruta(__dirname) dinamica y estan en la carpeta /views
 
+app.use(express.static(__dirname + '/public'));
 
-
-app.get('/', (req, res) =>{
-    res.render('index', { titulo : 'INICIO'});
-});
-
-app.get('/nosotros', (req, res) => {
-    res.render('nosotros', {titulo : 'Bienvenido a la sección nosotros'});
-});
+//llamamos el archivo creado de rutas
+app.use('/', require(__dirname + '/router/rutasWeb'));
 
 app.use( (req, res, next) => {
     res.status(404).render('404', {titulo404: 'Error404'});
