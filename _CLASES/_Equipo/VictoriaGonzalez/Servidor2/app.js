2f7a2cb8413d0f7d391650bea737a87d3788093e
VictoriaNadia <victoria.gonzalez@arbusta.net>
2022-07-06 23:13:33 -0300

    vicky

--- _CLASES/_Equipo/VictoriaGonzalez/Servidor2/app.js
+++ _CLASES/_Equipo/VictoriaGonzalez/Servidor2/app.js
@@ -4,25 +4,18 @@ const app = express();  // metodos express
 const port = 3000;   // definimos el puerto
 
 // defino el motor de vistas EJS
-app.set("view engine", "ejs"); //cual va a ser mi motor de vistas
-app.set("views", __dirname + "/views"); // donde van a estar esas vistas
+app.set("view engine", "ejs"); 
+app.set("views", __dirname + "/views"); 
+app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
-  res.render("index", { titulo: "inicio EJS" , h1: "h1 index en EJS de Victoria Gonzalez"});
+//llamar archivo rutas
+app.use('/', require('./router/RutasWeb'));
+
+app.use((req, res, next) => {
+  res.status(404).render("404",{titulo: "Error 404, intente más tarde.."});
 });
-
-app.get("/nosotros", (req, res) => {
-  res.render("nosotros", { titulo: "Nosotros EJS" , h1: "h1 nosotros en EJS de Victoria Gonzalez"});
-});
-
-
-
- app.use((req, res, next) => {
-  res.status(404).render("404", { titulo: "Página 404", h1: "Página no encontrada, reintente de Victoria Gonzalez :)" });
-}); 
-
 
 
 app.listen(port, () => {
-  console.log(`Ejemplo , levanto mi servidor en : http://localhost:${port}`);
+  console.log(`Servidor funcionando http://localhost:${port}`);
 });
