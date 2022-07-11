cbae791c395705a224634fe5099a32eb17ffae0b
Kevin Aviles <kevin.aviles@arbusta.net>
2022-07-11 16:43:11 -0300

    Entrega del ejercicio Router

--- _CLASES/_Equipo/kevinAviles/Ejercicios 1 y 2/app.js
+++ _CLASES/_Equipo/kevinAviles/Ejercicios 1 y 2/app.js
@@ -20,18 +20,20 @@ server.listen(port,()=>{
 const express = require("express"); // Requerimos Express
 const app = express();  // metodos express
 const port = 3000;   // definimos el puerto
-
+const rutasWeb= require('./routers/rutasWeb');
 // defino el motor de vistas EJS
 app.set("view engine", "ejs"); //cual va a ser mi motor de vistas
 app.set("views", __dirname + "/views"); // donde van a estar esas vistas
 
-app.get("/", (req, res) => {
+/* app.get("/", (req, res) => {
   res.render("index", { titulo: "inicio EJS" , h1: "h1 index en EJS"});
 });
 
 app.get("/nosotros", (req, res) => {
   res.render("nosotros", { titulo: "Nosotros EJS" , h1: "h1 nosotros en EJS"});
 });
+ */
+app.use('/',rutasWeb);
 
 /* Definir la vista para el error 404 */
 app.use((req, res, next) => {
