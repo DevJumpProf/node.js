9ec1f8b1c86a1a107e0861b0028f5fe1ade4caf3
Arbusta <gabriel.munoz@arbusta.net>
2022-07-11 11:42:01 -0300

    mvc

--- _CLASES/servidor/router/cursos.js
+++ _CLASES/servidor/router/cursos.js
@@ -1,10 +1,9 @@
 const express = require('express'); //requerimos express
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    // console.log(__dirname)
-    res.render("index", {titulo:"Pagina: Index"})
-})
+const cursosController = require('../controllers/cursosController');
+
+router.get('/', cursosController.index);
 
 
 module.exports = router;
