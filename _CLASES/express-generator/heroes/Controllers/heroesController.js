724836d5e5e8888b582ba9bfda2a0af61bbe51b1
Arbusta <gabriel.munoz@arbusta.net>
2022-07-13 17:21:08 -0300

    heroes TP

--- _CLASES/express-generator/heroes/Controllers/heroesController.js
+++ _CLASES/express-generator/heroes/Controllers/heroesController.js
@@ -1,4 +1,10 @@
-module.exports ={
+const fs = require ("fs");
+/* console.log(fs) */
+const heroes = JSON.parse (fs.readFileSync("./data/heroes.json", "utf8"));
 
-    
+module.exports ={
+    index: (req,res) =>{
+        res.send (heroes)
+    }
 }
+
