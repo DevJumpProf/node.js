357fe2086155cca0fe65ce7839c70f2a6d50c12e
Kevin Aviles <kevin.aviles@arbusta.net>
2022-07-15 17:03:54 -0300

    parte del tp 2 y ejercicio en clase

--- _CLASES/_Equipo/kevinAviles/express-generator/heroes/controllers/heroesController.js
+++ _CLASES/_Equipo/kevinAviles/express-generator/heroes/controllers/heroesController.js
@@ -4,37 +4,63 @@ const heroes = JSON.parse(fs.readFileSync("./data/heroes.json", "utf8"));
 
 module.exports = {
     index: (req, res) => {
-        res.send(heroes)
+        res.render('heroes',{
+            titulo:'Nuestros Heroes',
+            heroes:heroes
+        })
+        /* res.send(heroes) */
     },
     detalle: (req, res) => {
+
   let idHeroe = req.params.id; 
      /*    let { idHeroe } = req.params */
 
         heroes.forEach(heroe => {
             if (idHeroe == heroe.id) {
-                res.send(`hola soy heroe ${heroe.nombre} y mi profesion es ${heroe.profesion}`)
+               /*  res.send(`hola soy heroe ${heroe.nombre} y mi profesion es ${heroe.profesion}`) */
+               res.render('detalleHeroe',{
+                titulo:`Detalle heroe ${heroe.id}`,
+                heroe:heroe
+               })
             }
         });
-        res.send("no encontramos tu heroe")
+        res.render('404',{
+            titulo:"ERROR",
+            message:`Error, no encontramos al heroe num ${req.params.id}`
+        })
+        /* res.send("no encontramos tu heroe") */
     },
     bio: (req, res) => {
         let idHeroe = req.params.id;
         let ok = req.params.ok;
-res.set ({"content-type":"text/plain;charset=utf-8"})
+/* res.set ({"content-type":"text/plain;charset=utf-8"}) */
 
         heroes.forEach(heroe => {
             if (heroe.id == idHeroe) {
-                if (ok == "ok") {
-                    res.write(`hola, mi nombre es : ${heroe.nombre}`)
+                if (ok == "ok" || ok == "no") {
+                    res.render('bioHeroe',{
+                        titulo: `Bio del Heroe ${heroe.nombre}`,
+                        heroe:heroe,
+                        estado:ok
+                    })
+                   /*  res.write(`hola, mi nombre es : ${heroe.nombre}`)
                     res.write(heroe.resenia)
-                    res.end()
-                } else {
-                    res.send(`${heroe.nombre} dice que lamenta que no quieras ver su bio`)
+                    res.end() */
                 }
+                res.render('404',{
+                    titulo:"ERROR",
+                    message:`Error la ruta ${req.url} no existe`
+                })
+                    /* res.send(`${heroe.nombre} dice que lamenta que no quieras ver su bio`) */
+                
 
             }
         });
-        res.send("no encontramos tu heroe")
+        res.render('404',{
+            titulo:"ERROR",
+            message:`Error, no encontramos al heroe num ${req.params.id}`
+        })
+       /*  res.send("no encontramos tu heroe") */
     }
 }
 
