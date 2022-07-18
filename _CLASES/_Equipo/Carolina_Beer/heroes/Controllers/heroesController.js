cefe57b73b78ddba694427c01df0ef4582a7b52b
BeerCarolina <carolina.beer@arbusta.net>
2022-07-18 12:55:53 -0300

    tp heroes2 Beer

--- _CLASES/_Equipo/Carolina_Beer/heroes/Controllers/heroesController.js
+++ _CLASES/_Equipo/Carolina_Beer/heroes/Controllers/heroesController.js
@@ -4,7 +4,23 @@ const heroes = JSON.parse (fs.readFileSync("./data/heroes.json", "utf8"));
 
 module.exports ={
     index: (req,res) =>{
-        res.send (heroes)
+        res.render ('heroes', {heroes:heroes})
+    },
+
+
+
+    show: (req, res, next) => {
+        let {id} = req.params;
+
+        heroes.forEach(heroes => {
+            if(id == heroes.id){
+                res.send(`Soy ${heroes.nombre} mi profesion es ${heroes.profesion} y soy de pais:${heroes.pais}`)
+            }
+        })
+        res.send('No encontramos al heroe solicitado')
     }
+     
+    
+    
 }
 
--- _CLASES/_Equipo/Carolina_Beer/heroes/routes/heroes.js
+++ _CLASES/_Equipo/Carolina_Beer/heroes/routes/heroes.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const heroesController = require ("../controllers/heroesController")
 
 router.get("/",heroesController.index)
+router.get("/:id", heroesController.show)
 
 
 module.exports = router
