ffec1eb508e2ee00b39da810d0dfd6d6c8d164da
VictoriaNadia <victoria.gonzalez@arbusta.net>
2022-07-28 11:56:27 -0300

    Vicky

---
+++ _CLASES/_Equipo/VictoriaGonzalez/E-learning/controllers/RegisterController.js
@@ -0,0 +1,16 @@
+// import RegisterModel from "../models/RegisterModel.js"
+
+
+
+export const index = (req, res) =>{
+     res.render("register", {title: "Registro"})
+}
+
+
+
+
+
+
+
+
+
---
+++ _CLASES/_Equipo/VictoriaGonzalez/E-learning/routes/register.js
@@ -0,0 +1,9 @@
+import express from "express"
+
+import {index}  from "../controllers/RegisterController.js"
+
+const router = express.Router()
+
+router.get("/", index)
+
+export default router 
