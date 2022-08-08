c1c6954e47fd4356c533aa8bbd57c075cdd19064
maxiPortelBeraud <maximiliano.portel@arbusta.net>
2022-08-08 11:41:30 -0300

    ejercicio multer

---
+++ _CLASES/_Equipo/maxiPortel/22_02-08/Login_JWT_MySQL/routes/router.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+const multer = require('multer');
+const path = require('path');
+const authController = require('../controllers/authController')
+
+//configuracion de multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/img/tmp/my-uploads')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix+path.extname(file.originalname))
+    }
+})
+
+const upload = multer({ storage: storage })
+
+//router para las vistas
+router.get('/', authController.isAuthenticated, (req, res) => {
+    res.render('index', { user: req.user })
+})
+router.get('/login', (req, res) => {
+    res.render('login', { alert: false })
+})
+router.get('/register', (req, res) => {
+    res.render('register')
+})
+
+
+//router para los métodos del controller
+router.post('/register',upload.any(), authController.register)
+router.post('/login', authController.login)
+router.get('/logout', authController.logout)
+
+module.exports = router
