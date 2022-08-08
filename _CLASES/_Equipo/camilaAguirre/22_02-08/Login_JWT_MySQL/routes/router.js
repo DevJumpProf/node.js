7578649e2d9d9f10d0bf53109c1465fc3b266430
camiAguirre4rb1 <camila.aguirre@arbusta.net>
2022-08-08 16:09:05 -0300

    multer

--- _CLASES/_Equipo/camilaAguirre/22_02-08/Login_JWT_MySQL/routes/router.js
+++ _CLASES/_Equipo/camilaAguirre/22_02-08/Login_JWT_MySQL/routes/router.js
@@ -1,7 +1,23 @@
 const express = require('express')
+const multer = require('multer')
+const path = require('path'); /* se requiere path y se lo utiliza en la config. de multer */
 const router = express.Router()
 
 const authController = require('../controllers/authController')
+
+//configuracion subida de archivos-multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+    /*     console.log(file) */
+      cb(null, 'tmp/my-uploads') /* se indica en que carpeta se debera guardar los archivos */
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname)) /* con path indicamos que nos debe traer la extension del archivo subido */
+    }
+  })
+  
+  const upload = multer({ storage: storage })
 
 //router para las vistas
 router.get('/', authController.isAuthenticated, (req, res)=>{    
@@ -16,7 +32,7 @@ router.get('/register', (req, res)=>{
 
 
 //router para los métodos del controller
-router.post('/register', authController.register)
+router.post('/register',upload.any(),authController.register)  /* en la ruta se debe indicar la subida de archivos */
 router.post('/login', authController.login)
 router.get('/logout', authController.logout)
 
