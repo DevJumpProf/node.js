7a31bb270ea91869291f1970b02fe443ddcd836d
Eze Rodriguez <ezequiel.rodriguez@arbusta.net>
2022-08-09 11:08:35 -0300

    middlewares 09-08

--- _CLASES/_Equipo/Ezequiel Rodriguez/Tarea 03-08/Login_JWT_MySQL/routes/router.js
+++ _CLASES/_Equipo/Ezequiel Rodriguez/Tarea 03-08/Login_JWT_MySQL/routes/router.js
@@ -1,22 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const authController = require('../controllers/authController.js')
-const multer = require("multer")
-const path = require("path")
-
-// Config Multer - DiskStorage
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'public/img')
-    },
-    filename: function (req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname)) // Sumar extension del archivo (path.extname())
-    }
-  })
-  
-const upload = multer({ storage: storage })
-
+const uploadUser = require("../middlewares/uploadUserAvatar.js")
 
 //router para las vistas
 router.get('/', authController.isAuthenticated, (req, res)=>{    
@@ -30,10 +15,9 @@ router.get('/register', (req, res)=>{
     res.render('register')
 })
 
-
 //router para los métodos del controller
-router.post('/register', upload.any(), authController.register)
+router.post('/register', uploadUser.any(), authController.register)
 router.post('/login', authController.login)
 router.get('/logout', authController.logout)
 
-module.exports = router
+module.exports = router;
