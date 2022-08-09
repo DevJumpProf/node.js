29a55c62d5ecd06d5b2fdf2a16a094842530fc82
Kevin Aviles <kevin.aviles@arbusta.net>
2022-08-09 09:20:08 -0300

    subida de archivos

---
+++ _CLASES/_Equipo/kevinAviles/22_2-08/Login_JWT_MySQL/models/UserModels.js
@@ -0,0 +1,11 @@
+/* const db = require('../database/db.js');
+
+const { DataTypes } = require('sequelize');
+
+const UserModel = db.define('heroes',{
+    user : {type: DataTypes.STRING},
+    name : {type: DataTypes.STRING},
+    pass : {type: DataTypes.STRING}
+})
+
+module.exports = UserModel;  */
---
+++ _CLASES/_Equipo/kevinAviles/22_2-08/Login_JWT_MySQL/routes/router.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+
+const authController = require('../controllers/authController')
+
+
+router.get('/login', (req, res)=>{
+    res.render('login', {alert:false})
+})
+router.get('/register', (req, res)=>{
+    res.render('register')
+})
+
+
+//router para los métodos del controller
+router.post('/register', authController.register)
+router.post('/login', authController.login)
+router.get('/logout', authController.logout)
+
+//router para las vistas
+router.get('/', authController.isAuthenticated, (req, res)=>{    
+    res.render('index', {user:req.user})
+})
+module.exports = router
