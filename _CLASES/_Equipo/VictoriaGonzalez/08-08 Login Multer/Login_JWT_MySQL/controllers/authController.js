724d1566fbf7bcb75a7fd1f119ac0a147e0278ff
VictoriaNadia <victoria.gonzalez@arbusta.net>
2022-08-10 21:38:25 -0300

    victoria

--- _CLASES/_Equipo/VictoriaGonzalez/08-08 Login Multer/Login_JWT_MySQL/controllers/authController.js
+++ _CLASES/_Equipo/VictoriaGonzalez/08-08 Login Multer/Login_JWT_MySQL/controllers/authController.js
@@ -13,8 +13,9 @@ exports.register = async (req, res, next)=>{
         const user = req.body.user
         const pass = await bcryptjs.hash(req.body.pass, 10)  
         const avatar = req.files[0].filename
+        const email = req.body.email
         //console.log(passHash)   
-        conexion.query('INSERT INTO users SET ?', {user:user, name: name, pass:pass, avatar:avatar}, (error, results)=>{
+        conexion.query('INSERT INTO users SET ?', {user:user, name: name, pass:pass, avatar:avatar, email:email}, (error, results)=>{
             if(error){console.log(error)}
             res.redirect('/')
         })
