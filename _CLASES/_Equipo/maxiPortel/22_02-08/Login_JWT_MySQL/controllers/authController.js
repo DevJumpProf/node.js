e8ea96da8b0579c818731b413d12f9b7ffa9c1cb
maxiPortelBeraud <maximiliano.portel@arbusta.net>
2022-08-12 11:08:08 -0300

    sequelizando

--- _CLASES/_Equipo/maxiPortel/22_02-08/Login_JWT_MySQL/controllers/authController.js
+++ _CLASES/_Equipo/maxiPortel/22_02-08/Login_JWT_MySQL/controllers/authController.js
@@ -1,42 +1,30 @@
 const jwt = require('jsonwebtoken')
 const bcryptjs = require('bcryptjs')
-const conexion = require('../database/db')
-const { promisify } = require('util')
-const {check, validationResult, body} = require('express-validator');
-
+const { promisify } = require('util');
+const userModel = require('../models/userModel.js');
+const { check, validationResult, body } = require('express-validator');
 
 //procedimiento para registrarnos
 exports.register = async (req, res, next) => {
 
     let errors = validationResult(req);
-    if(errors.isEmpty()){
-    try {
-        const name = req.body.name
-        const user = req.body.user
-        const pass = await bcryptjs.hash(req.body.pass, 10)
-        const avatar = req.files[0].filename
-        const email = req.body.email
+    /* console.log(errors); */
+    if (errors.isEmpty()) {
+        try {
+            const name = req.body.name
+            const user = req.body.user
+            const pass = await bcryptjs.hash(req.body.pass, 10)
+            const avatar = req.files[0].filename
+            const email = req.body.email
 
-        conexion.query('SELECT user FROM users WHERE user = ?', user.toLowerCase(), async (err, resu) => {
-            if( resu.length !== 0 ){
-                console.log('ERROR');
-                res.send('ERROR USUARIO YA CREADO')
-            }else{
-                
-                conexion.query('INSERT INTO users SET ?', { user: user.toLowerCase(), name: name, pass: pass, avatar: avatar, email: email}, (error, results) => {
-                    if (error) { console.log(error) }
-                    res.redirect('/')
-                })
-            }
-        })
-        //console.log(passHash)   
-
-    } catch (error) {
-        console.log(error)
+            await userModel.create({ user: user.toLowerCase(), name: name, pass: pass, avatar: avatar, email: email });
+            res.redirect('/')
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        return res.render('register', { errors: errors.errors })
     }
-}else{
-    return res.render('register', {errors: errors.errors})
-}
 }
 
 exports.login = async (req, res) => {
@@ -55,43 +43,44 @@ exports.login = async (req, res) => {
                 ruta: 'login'
             })
         } else {
-            conexion.query('SELECT * FROM users WHERE user = ?', [user], async (error, results) => {
-                if (results.length == 0 || !(await bcryptjs.compare(pass, results[0].pass))) {
-                    res.render('login', {
-                        alert: true,
-                        alertTitle: "Error",
-                        alertMessage: "Usuario y/o Password incorrectas",
-                        alertIcon: 'error',
-                        showConfirmButton: true,
-                        timer: false,
-                        ruta: 'login'
-                    })
-                } else {
-                    //inicio de sesión OK
-                    const id = results[0].id
-                    const token = jwt.sign({ id: id }, process.env.JWT_SECRETO, {
-                        expiresIn: process.env.JWT_TIEMPO_EXPIRA
-                    })
-                    //generamos el token SIN fecha de expiracion
-                    //const token = jwt.sign({id: id}, process.env.JWT_SECRETO)
-                    console.log("TOKEN: " + token + " para el USUARIO : " + user)
-
-                    const cookiesOptions = {
-                        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
-                        httpOnly: true
-                    }
-                    res.cookie('jwt', token, cookiesOptions)
-                    res.render('login', {
-                        alert: true,
-                        alertTitle: "Conexión exitosa",
-                        alertMessage: "¡LOGIN CORRECTO!",
-                        alertIcon: 'success',
-                        showConfirmButton: false,
-                        timer: 800,
-                        ruta: ''
-                    })
-                }
+            const usuarioPass = await userModel.findAll({
+                where: { user: user }
             })
+            if (usuarioPass.length == 0 || !(await bcryptjs.compare(pass, usuarioPass[0].pass))) {
+                res.render('login', {
+                    alert: true,
+                    alertTitle: "Error",
+                    alertMessage: "Usuario y/o Password incorrectas",
+                    alertIcon: 'error',
+                    showConfirmButton: true,
+                    timer: false,
+                    ruta: 'login'
+                })
+            } else {
+                //inicio de sesión OK
+                const id = usuarioPass[0].id
+                const token = jwt.sign({ id: id }, process.env.JWT_SECRETO, {
+                    expiresIn: process.env.JWT_TIEMPO_EXPIRA
+                })
+                //generamos el token SIN fecha de expiracion
+                //const token = jwt.sign({id: id}, process.env.JWT_SECRETO)
+                console.log("TOKEN: " + token + " para el USUARIO : " + user)
+
+                const cookiesOptions = {
+                    expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
+                    httpOnly: true
+                }
+                res.cookie('jwt', token, cookiesOptions)
+                res.render('login', {
+                    alert: true,
+                    alertTitle: "Conexión exitosa",
+                    alertMessage: "¡LOGIN CORRECTO!",
+                    alertIcon: 'success',
+                    showConfirmButton: false,
+                    timer: 800,
+                    ruta: ''
+                })
+            }
         }
     } catch (error) {
         console.log(error)
@@ -102,11 +91,12 @@ exports.isAuthenticated = async (req, res, next) => {
     if (req.cookies.jwt) {
         try {
             const decodificada = await promisify(jwt.verify)(req.cookies.jwt, process.env.JWT_SECRETO)
-            conexion.query('SELECT * FROM users WHERE id = ?', [decodificada.id], (error, results) => {
-                if (!results) { return next() }
-                req.user = results[0]
-                return next()
+            const userId = await userModel.findAll({
+                where: { id: decodificada.id }
             })
+            if (!userId) { return next() }
+            req.user = userId[0]
+            return next()
         } catch (error) {
             console.log(error)
             return next()
--- _CLASES/_Equipo/maxiPortel/22_02-08/Login_JWT_MySQL/validations/validatorRegister.js
+++ _CLASES/_Equipo/maxiPortel/22_02-08/Login_JWT_MySQL/validations/validatorRegister.js
@@ -1,8 +1,41 @@
-const {check, validationResult, body} = require('express-validator');
+const { check, validationResult, body } = require('express-validator');
+const userModel = require('../models/userModel.js');
 
 module.exports = [
     check('name').isLength().withMessage('Ingrese nombre'),
-    check('user').isLength({min: 4}).withMessage('El usuario debe tener minimo 4 caracteres'),
-    check('pass').isLength({min: 8}).withMessage('La contraseña debe tener minimo 8 caracteres'),
-    check('email').isEmail().withMessage('Ingrese email válido')
-    ]
+    check('user').isLength({ min: 4 }).withMessage('El usuario debe tener minimo 4 caracteres'),
+    check('pass').isLength({ min: 8 }).withMessage('La contraseña debe tener minimo 8 caracteres'),
+    check('email').isEmail().withMessage('Ingrese email válido'),
+    /* body('user').custom(function(user){
+        return new Promise((resolve,reject) => {
+            const userValidation = userModel.findAll({
+                where: { user: user }
+            })
+                if(!userValidation.length == 0){
+                    reject(new Error('El usuario ya existe'))
+                }
+                resolve()
+        })
+    }).withMessage('Usuario ya registrado') */
+    body('user')
+        .custom(function (value) {
+            return userModel.findOne({
+                where: {
+                    user: value
+                }
+            })
+                .then(user => {
+                    if (user) {
+                        return Promise.reject('Este usuario ya se encuentra registrado')
+                    }
+                })
+        }),
+    body('pass2')
+        .custom((value, { req }) => {
+            if (value != req.body.pass) {
+                return false
+            }
+            return true
+        })
+        .withMessage("Las constraseñas no coiciden"),
+]
