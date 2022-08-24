67058e8d8c5e43fe46d5e1636955afe7d7050009
Eze Rodriguez <ezequiel.rodriguez@arbusta.net>
2022-08-24 09:47:46 -0300

    Test Typescript

---
+++ _CLASES/_Equipo/Ezequiel Rodriguez/Test TypeScript/src/03-typing.ts
@@ -0,0 +1,13 @@
+(() => {
+  let myProductoName = 'Product 1';
+  let myProductoPrice = 123;
+})();
+
+myProductoName = 'change';
+
+myProductoName.toLowerCase();
+
+myProductoPrice.toFixed();
+
+const myProductoStock = 1000;
+const myProductoName2 = "Product 1";
---
+++ _CLASES/_Equipo/Ezequiel Rodriguez/Test TypeScript/src/app.ts
@@ -0,0 +1,35 @@
+(async ()=> {
+  const myCart = [];
+  const products: string | any[] = [];
+  const limit = 2;
+  async function getProducts() {
+    const rta = await fetch('http://api.escuelajs.co/api/v1/products', {
+      method: 'GET'
+    });
+    const data = await rta.json();
+    products.concat(data);
+  }
+  function getTotal() {
+    let total = 0;
+    for (let i = 0; i < products.length; i++) {
+      total += products[i].price;
+    }
+    return total;
+  }
+  async function addProduct(index: number) {
+    if (getTotal() <= limit) {
+      myCart.push(products[index]);
+    }
+  }
+await getProducts();
+  addProduct(1);
+  addProduct(2);
+  const total = getTotal();
+  console.log(total);
+  const person = {
+    name: 'Nicolas',
+    lastName: 'Molina'
+  }
+  /* const rta = person +  limit;
+  console.log(rta); */
+});
