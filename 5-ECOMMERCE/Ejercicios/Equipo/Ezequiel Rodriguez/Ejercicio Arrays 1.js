b4797226aa8f2f4122b8d78516db9299e019512d
Eze Rodriguez <ezequiel.rodriguez@arbusta.net>
2022-11-09 10:05:55 -0300

    Ejercicios array 1 - ezequiel

---
+++ 5-ECOMMERCE/Ejercicios/Equipo/Ezequiel Rodriguez/Ejercicio Arrays 1.js
@@ -0,0 +1,71 @@
+/* Ejercicio  arrays */
+
+/* 1)Declarar un array que vamos a llamar “clasificaciones” con los siguientes valores:
+Marcos, Franco, Agostina, Leon, Juan Cruz,Eduardo */
+
+let clasificaciones = [
+  "Marcos",
+  "Franco",
+  "Agostina",
+  "Leon",
+  "Juan Cruz",
+  "Eduardo",
+];
+
+/* (vamos a suponer que esa es la orden de clasificación de un concurso) */
+
+/* 2)recorre el array (clasificaciones) e Imprime la clasificación actual.
+ (realizar una funcion "mostrarClasificacion()" esa funcion debe recorrer el array con un bucle for y mostrar la clasificacion) */
+function mostrarClasificacion(clasificaciones) {
+  // FORMA CON FOR
+  // for(let i=0;i<clasificaciones.length;i++){
+  //     console.log(`${i+1}-${clasificaciones[i]}`);
+  // }
+
+  // FORMA CON FOREACH
+  clasificaciones.forEach((element, i) => {
+    console.log(`${i + 1} - ${element}`);
+  });
+}
+//1era vista de la clasificacion
+// mostrarClasificacion(clasificaciones);
+
+
+/* 3)El concurso continua, y se van modifican esas posiciones anteriores. Debemos cambiar en el array: */
+/* a)Leon adelanta a Agostina */
+function adelantarAlumnos(clasificaciones){
+   clasificaciones.splice(2, 1, "Leon");
+   clasificaciones.splice(3, 1, "Agostina");
+   console.log(clasificaciones);
+}
+adelantarAlumnos(clasificaciones);
+/* b)Eduardo es descalificado y se elimina del concurso */
+function descalificarAlumno(clasificaciones) {
+  clasificaciones.pop();
+  return clasificaciones;
+}
+descalificarAlumno(clasificaciones)
+/* c)Detrás de Marcos y antes de Franco se clasifican dos nuevas concursantes: Julieta y Martina, en ese orden */
+function nuevosConcursantes(clasificaciones){
+    // Agregar nuevos concursantes
+    clasificaciones.push("Julieta","Martina");
+
+    // Modificar posiciones de los nuevos concursantes
+    clasificaciones.splice(1,1,"Julieta");
+    clasificaciones.splice(2,1,"Martina");
+    clasificaciones.splice(clasificaciones.length-1,1,"Juan Cruz");
+    clasificaciones.splice(clasificaciones.length-2,1,"Agostina");
+    clasificaciones.splice(clasificaciones.length-3,1,"Leon");
+    clasificaciones.splice(clasificaciones.length-4,1,"Franco");
+}
+nuevosConcursantes(clasificaciones);
+
+/* d)Hay una nueva participante que pasa a encabezar la clasificación: Alicia */
+function nuevaCabeza(clasificaciones) {
+  clasificaciones.unshift("Alicia");
+  return clasificaciones;
+}
+console.log(nuevaCabeza(clasificaciones));
+/* e)muestra la clasificación actualizada (mostrarClasificacion())y comprueba que se ha hecho correctamente  */
+// Vista final de la clasificacion (ver dentro de la funcion)
+mostrarClasificacion(clasificaciones);
---
+++ 5-ECOMMERCE/Ejercicios/Equipo/Ezequiel Rodriguez/Ejercicio Arrays 2.js
@@ -0,0 +1,176 @@
+// Arrays
+let marcas = ["Renault","Dior","Givenchy"];
+let numeros = [12,232,34,2,46];
+let frutas = ["banana", "pera", "manzana"];
+
+function devolverPrimerElemento(array) {
+  // Devuelve el primer elemento de un array
+  // Tu código:
+  return array[0];
+}
+console.log(devolverPrimerElemento(marcas));
+
+
+function devolverUltimoElemento(array) {
+  // Devuelve el último elemento de un array
+  // Tu código:
+  return array[array.length-1];
+}
+console.log(devolverUltimoElemento(marcas));
+
+
+
+function obtenerLargoDelArray(array) {
+  // Devuelve el largo de un array
+  // Tu código:
+  return array.length;
+}
+console.log(obtenerLargoDelArray(marcas));
+
+function incrementarPorUno(array) {
+  // "array" debe ser una matriz de enteros (int/integers)
+  // Aumenta cada entero por 1
+  // y devuelve el array
+  // Tu código:
+  let nuevosNumeros = array.map(elemento => elemento + 1);
+  return nuevosNumeros;
+}
+  console.log(incrementarPorUno(numeros));
+
+function agregarItemAlFinalDelArray(array, elemento) {
+  // Añade el "elemento" al final del array
+  // y devuelve el array
+  // Tu código:
+  array.push(elemento);
+  return array;
+}
+  console.log(agregarItemAlFinalDelArray(marcas, "Gucci"));
+
+function agregarItemAlComienzoDelArray(array, elemento) {
+  // Añade el "elemento" al comienzo del array
+  // y devuelve el array
+  // Pista: usa el método `.unshift`
+  // Tu código:
+  array.unshift(elemento);
+  return array;
+}
+  console.log(agregarItemAlComienzoDelArray(frutas, "frutilla"));
+
+function dePalabrasAFrase(palabras) {
+  // "palabras" es un array de strings/cadenas
+  // Devuelve un string donde todas las palabras estén concatenadas
+  // con espacios entre cada palabra
+  // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
+  // Tu código:
+  let juntos = palabras.join(" ");
+  return juntos;
+}
+  console.log(dePalabrasAFrase(marcas));
+
+
+function arrayContiene(array, elemento) {
+  // Comprueba si el elemento existe dentro de "array"
+  // Devuelve "true" si está, o "false" si no está
+  // Tu código:
+  return array.includes(elemento);
+}
+  console.log(arrayContiene(frutas, "pera"));
+
+function agregarNumeros(numeros) {
+  // "numeros" debe ser un arreglo de enteros (int/integers)
+  // Suma todos los enteros y devuelve el valor
+  // Tu código:
+  let sumados = numeros.reduce((ac,actual) => ac + actual);
+  return sumados
+}
+  console.log(agregarNumeros(numeros));
+
+
+function promedioResultadosTest(resultadosTest) {
+  // "resultadosTest" debe ser una matriz de enteros (int/integers)
+  // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
+  // Tu código:
+}
+
+
+function numeroMasGrande(numeros) {
+  // "numeros" debe ser una matriz de enteros (int/integers)
+  // Devuelve el número más grande
+  // Tu código:
+}
+
+
+function multiplicarArgumentos() {
+  // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
+  // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
+  // Escribe tu código aquí:
+}
+
+
+function cuentoElementos(arreglo){
+  //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
+  //Escribe tu código aquí
+
+}
+
+
+function diaDeLaSemana(numeroDeDia) {
+  //Suponga que los días de la semana se codifican como 1 = Domingo, 2 = Lunes y así sucesivamente. 
+  //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
+  //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
+  //Escribe tu código aquí   
+  
+} 
+
+
+function empiezaConNueve(n) {
+  //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
+  //inicia con 9 y false en otro caso.
+  //Escribe tu código aquí
+  
+}
+
+
+function todosIguales(arreglo) {
+  //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
+  //retornar true, caso contrario retornar false.
+  //Escribe tu código aquí  
+  
+} 
+
+
+function mesesDelAño(array) {
+  //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de 
+  // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
+  //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
+  // Tu código:
+}
+
+
+function mayorACien(array) {
+  //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
+  //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
+  // Tu código:
+}
+
+
+function breakStatement(numero) {
+  //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
+  //Guardar cada nuevo valor en un array. 
+  //Devolver el array
+  //Si en algún momento el valor de la suma y la cantidad de iteraciones coinciden, debe interrumpirse la ejecución y 
+  //devolver: "Se interrumpió la ejecución"
+  //Pista: usá el statement 'break'
+  // Tu código:
+}
+
+
+function continueStatement(numero) {
+  //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
+  //Guardar cada nuevo valor en un array.    
+  //Devolver el array
+  //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
+  //Pista: usá el statement 'continue'
+  // Tu código:
+}
+
